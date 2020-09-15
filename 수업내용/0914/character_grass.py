2bebcb030b31b9b1f18fbe0d8c95720bc420075f
naioooo <34563332+naioooo@users.noreply.github.com>
2020-09-15 21:37:11 +0900

    김민규
    김민규

--- 수업내용/0914/character_grass.py
+++ 수업내용/0914/character_grass.py
@@ -2,8 +2,8 @@ from pico2d import *
 
 open_canvas()
 
-grass = load_image('res/grass.png')
-character = load_image('res/character.png')
+grass = load_image('../res/grass.png')
+character = load_image('../res/character.png')
 
 grass.draw_now(400, 30)
 character.draw_now(400, 90)
