7d13c49f507249def88851db889889ea96ef781c
naioooo <34563332+naioooo@users.noreply.github.com>
2020-11-15 00:23:09 +0900

    추가

--- 기말프로젝트/title_state.py
+++ 기말프로젝트/title_state.py
@@ -1,20 +1,15 @@
 import gfw
 from pico2d import *
-import title_state
+import main_state
 
 
 def enter():
-    global image, elapsed
-    image = load_image('res/kpu_credit.png')
-    elapsed = 0
+    global image
+    image = load_image('res/title.png')
 
 
 def update():
-    global elapsed
-    elapsed += gfw.delta_time
-    print(elapsed)
-    if elapsed > 1.0:
-        gfw.change(title_state)
+    pass
 
 
 def draw():
@@ -26,6 +21,8 @@ def handle_event(e):
         gfw.quit()
     elif (e.type, e.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
         gfw.quit()
+    elif (e.type, e.key) == (SDL_KEYDOWN, SDLK_SPACE):
+        gfw.push(game_state)
 
 
 def exit():
