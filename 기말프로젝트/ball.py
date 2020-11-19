ab96b30e8deed03a10f81163ca86cb330bfea596
naioooo <34563332+naioooo@users.noreply.github.com>
2020-11-20 00:59:47 +0900

    물리 구현

--- 기말프로젝트/ball.py
+++ 기말프로젝트/ball.py
@@ -1,13 +1,15 @@
 from pico2d import *
 import gfw
+import random
 
 MOVE_POS = 300
 
+GRAVITY = 0.2
 
 def init():
     global image, pos, delta_x, delta_y, radius
     image = gfw.image.load('res/ball1.png')
-    pos = get_canvas_width() // 2, get_canvas_height() // 2 - 140
+    pos = get_canvas_width() // 2, get_canvas_height() // 2
     delta_x, delta_y = 0, 0
     radius = image.w // 2
 
@@ -17,9 +19,16 @@ def update():
     x, y = pos
     x += delta_x * MOVE_POS * gfw.delta_time
     y += delta_y * MOVE_POS * gfw.delta_time
+    delta_y -= GRAVITY
     hw, hh = image.w // 2, image.h // 2
-    x = clamp(hw, x, get_canvas_width() - hw)
-    y = clamp(hh, y, get_canvas_height() - hh)
+
+    bottom = y - radius
+    if bottom < get_canvas_height() // 2 - 225 and delta_y < 0:
+        rand = random.randrange(8,9) / 10
+        delta_y *= -rand
+        if delta_y <= 1:
+            delta_y = 0
+
     pos = x, y
 
 
