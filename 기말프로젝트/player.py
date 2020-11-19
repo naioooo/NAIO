061f6743876491f185d7ac4286f8291cafed556d
naioooo <34563332+naioooo@users.noreply.github.com>
2020-11-19 23:52:58 +0900

    물리 구현

--- 기말프로젝트/player.py
+++ 기말프로젝트/player.py
@@ -9,13 +9,40 @@ class Player:
         self.x, self.y = get_canvas_width() // 2, get_canvas_height() // 2 - 200
         self.pos = self.x, self.y
         self.dx = 0
+        self.dy = 0
         self.speed = 200
         self.image =  gfw.image.load('res/pikacu1.png')
+        self.jump = False
+        self.jump_power = 0
+        self.jump_direction = 0
+
+    def jump_check(self):
+        if self.jump == True:
+            if self.jump_direction == 1:
+
+                self.dy =  self.jump_power
+                self.jump_power -= 1
+                if self.jump_power == 0:
+                    self.jump_direction = 2
+
+            elif self.jump_direction == 2:
+                self.dy = self.jump_power
+                self.jump_power -= 1
+                if self.y <= get_canvas_height() // 2 - 200:
+                    self.jump_direction = 0
+                    self.jump_power = 0
+                    self.dy = 0
+                    self.jump = False
+
+
 
 
     def update(self):
+        self.jump_check()
         self.x += self.dx * gfw.delta_time * self.speed
+        self.y += self.dy * gfw.delta_time * self.speed
         self.pos = self.x, self.y
+
 
 
 
@@ -30,7 +57,10 @@ class Player:
             elif e.key == SDLK_RIGHT:
                 self.dx += 1
             elif e.key == SDLK_UP:
-                pass
+                if self.jump == False:
+                    self.jump = True
+                    self.jump_power = 10
+                    self.jump_direction = 1
             elif e.key == SDLK_DOWN:
                 pass
 
